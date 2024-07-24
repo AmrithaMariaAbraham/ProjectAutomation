61b081bbf7c06de4d7014b98a1bfceec79041a27
amrit <amrit@192.168.1.39>
2024-07-24 07:59:47 +0530

    Added LinkText,PartialLinkText,cssSelector,xPath

--- AutomationProject/src/main/java/elementRepository/Locators.java
+++ AutomationProject/src/main/java/elementRepository/Locators.java
@@ -27,11 +27,33 @@ public class Locators extends Base
 	{
 		WebElement name=driver.findElement(By.name("states[]"));
 	}
+	public void linkText()
+	{
+		//Home-->Checkbox Demo(link)
+		WebElement element=driver.findElement(By.linkText("check-box-demo.php"));
+	}
+	public void partialLinkText()
+	{
+		WebElement element1=driver.findElement(By.partialLinkText("radio-button"));
+	}
+	public void cssSelector()
+	{
+		WebElement element2=driver.findElement(By.cssSelector("input#gridCheck"));//attribute tag#Id-->Method 1 for cssSelector
+		WebElement element3=driver.findElement(By.cssSelector("input.form-check-input")); //tag.class-->Method2 for cssSelector
+		WebElement element4=driver.findElement(By.cssSelector("input[class=form-check-input]"));//tag[attribute type=attribute value]-->Method3 for cssSelector
+		WebElement element5=driver.findElement(By.cssSelector("input.form-check-input[type=checkbox]")); //tag.class attribute value[attribute tag=attribute value]
+	}
+	public void xpath()
+	{
+		WebElement element6=driver.findElement(By.xpath("/html/body/section/div/div/div[2]/div[1]/div/div[2]/form/div[1]/div/input"));//Absolute xpath-->will get from root node
+		WebElement element7=driver.findElement(By.xpath("//input[@class='form-check-input']"));//relative xpath
+	}
 	public static void main(String[] args) 
 	{
 		Locators obj=new Locators();
 		obj.id();
 		obj.classId();
 		obj.name();
+		obj.linkText();
 	}
 }
